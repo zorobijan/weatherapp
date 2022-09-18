d349a86af492e889b903000f3b723e514a31fb71
Matthew Bascom <matt.bijan@gmail.com>
2022-09-18 18:20:18 -0500

    built skeleton

---
+++ script.js
@@ -0,0 +1,7 @@
+// define fetch parameters
+// fetch is the link+city name, API KEY
+// At the top we define API KEY with our personal API KEY (weatherAPIkey)
+// let city name - do not define
+// declare get weatherAPI function on event = getAPI(event) 
+// 
+console.log
