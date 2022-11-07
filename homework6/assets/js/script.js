78c678705d9c21e0e9c895e72d8682a70cba45c5
Matthew Bascom <matt.bijan@gmail.com>
2022-11-06 20:39:05 -0600

    pulling weather data, creating elements and appending

--- homework6/assets/js/script.js
+++ homework6/assets/js/script.js
@@ -16,21 +16,52 @@ const weatherOptions = {
 };
 
 function getApi(event) {
+  event.preventDefault()
   if (event.target.matches("button")) {
     city = document.getElementById("searchInput").value
     localStorage.setItem('searchInput', city)
     let queryUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`;
-    fetch(queryUrl, weatherOptions)
+    fetch(queryUrl)
       .then(function (response) {
-        return response.json();
         console.log(response)
+        return response.json();
       })
       .then(function (data) {
-        let weatherData = data.coord.lon;
-        console.log(weatherData);
+        console.log(data)
+        let lat = data.coord.lat;
+        let lon = data.coord.lon;
+        let temperature = data.main.temp;
+        let humidity = data.main.humidity;
+        let wind = data.wind.speed
+        let icon = data.weather[0].icon
+        console.log(lat);
+        console.log(lon);
+        console.log(temperature);
+        console.log(humidity);
+        console.log(wind);
+        console.log(icon);
       });
   }
 };
+
+let latEl = document.createElement("p");
+calorieEl.textContent = lat;
+forecast.appendChild(latEl);
+let lotEl = document.createElement("p");
+calorieEl.textContent = lon;
+forecast.appendChild(lotEl);
+let tempEl = document.createElement("p");
+calorieEl.textContent = temperature;
+forecast.appendChild(tempEl);
+let humidityEl = document.createElement("p");
+calorieEl.textContent = humidity;
+forecast.appendChild(humidityEl);
+let windEl = document.createElement("p");
+calorieEl.textContent = wind;
+forecast.appendChild(windEl);
+let iconEl = document.createElement("p");
+calorieEl.textContent = icon;
+forecast.appendChild(iconEl);
 
 button.addEventListener("click", getApi);
 
